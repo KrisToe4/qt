1c3819129638a86de5e33436174caca58c87c5a1
Kris Wilson <kristofor.wilson@gmail.com>
2018-02-03 06:41:22 -0800

    Final version of Hello World

--- Udemy_Tut_For_Beginners/HelloWorld/main.cpp
+++ Udemy_Tut_For_Beginners/HelloWorld/main.cpp
@@ -1,7 +1,16 @@
 #include <QApplication>
+#include <QLabel>
 
 int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
+
+    // GUI Elements
+    QLabel *label = new QLabel("Hello World");
+    label->setWindowTitle("My App");
+    label->resize(400, 400);
+    label->show();
+
+
     return app.exec();
 }
