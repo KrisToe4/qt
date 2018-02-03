57da476f5449a396d8e87410ca4dc479fab48df8
Kris Wilson <kristofor.wilson@gmail.com>
2018-02-03 08:43:31 -0800

    Check the current text/index of the combo box.

--- Udemy_Tut_For_Beginners/ComboBox/mainwindow.cpp
+++ Udemy_Tut_For_Beginners/ComboBox/mainwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QMessageBox>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -15,11 +17,16 @@ MainWindow::MainWindow(QWidget *parent) :
 
     for (int i = 0; i < 10; i++)
     {
-        ui->comboBox->addItem(QString::number(i) + "name");
+        ui->comboBox->addItem(QIcon(":/resource/img/green-light.png"), QString::number(i) + "name");
     }
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    QMessageBox::information(this, "Title", ui->comboBox->currentText());
 }
