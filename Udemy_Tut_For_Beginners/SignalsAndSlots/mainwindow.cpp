32e4987ca4a72b7b439dd0e6af765d0784214594
Kris Wilson <kristofor.wilson@gmail.com>
2018-02-03 07:14:48 -0800

    Changed to show how to choose buttons in a question type message box and handle a return value.

--- Udemy_Tut_For_Beginners/SignalsAndSlots/mainwindow.cpp
+++ Udemy_Tut_For_Beginners/SignalsAndSlots/mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QMessageBox>
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -26,5 +27,13 @@ void MainWindow::on_pushButton_clicked()
     //StandardButton    information
     //StandardButton    question
     //StardardButton    warning
-    QMessageBox::aboutQt(this, "My Message Box");
+    QMessageBox::StandardButton reply = QMessageBox::question(this, "My Message Box", "This is my custom message", QMessageBox::Yes | QMessageBox::No);
+    if (reply == QMessageBox::Yes)
+    {
+        QApplication::quit();
+    }
+    else
+    {
+        qDebug() << "No is clicked";
+    }
 }
