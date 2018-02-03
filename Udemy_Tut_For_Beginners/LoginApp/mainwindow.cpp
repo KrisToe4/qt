e5eaf4820914ff760221e8cb711a0274a46bc3f3
Kris Wilson <kristofor.wilson@gmail.com>
2018-02-03 07:45:30 -0800

    Add basic profile test

--- Udemy_Tut_For_Beginners/LoginApp/mainwindow.cpp
+++ Udemy_Tut_For_Beginners/LoginApp/mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +12,19 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButtonLogin_clicked()
+{
+    QString username = ui->lineEditUser->text();
+    QString password = ui->lineEditPass->text();
+
+    if ((username == "test") && (password == "test"))
+    {
+        QMessageBox::information(this, "Login", "Username and password are correct");
+    }
+    else
+    {
+        QMessageBox::warning(this, "Login", "Username or password are not correct");
+    }
 }
