026ff4d434dd053fbc84bf4e50e4cc4d2a1403d6
Kris Wilson <kristofor.wilson@gmail.com>
2018-02-03 08:41:37 -0800

    Add items programatically

--- Udemy_Tut_For_Beginners/ComboBox/mainwindow.cpp
+++ Udemy_Tut_For_Beginners/ComboBox/mainwindow.cpp
@@ -7,9 +7,16 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
 
+    /*
     ui->comboBox->addItem(QIcon(":/resource/img/green-light.png"), "Mark");
     ui->comboBox->addItem(QIcon(":/resource/img/red-light.png"), "John");
     ui->comboBox->addItem("Ralph");
+    */
+
+    for (int i = 0; i < 10; i++)
+    {
+        ui->comboBox->addItem(QString::number(i) + "name");
+    }
 }
 
 MainWindow::~MainWindow()
